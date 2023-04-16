7fdb22135e6906b76c56be3e86aff13218ed6fc1
andinhodantas <andersonnascimentodantas@gmail.com>
2023-04-16 15:35:51 -0300

    pagina inicial

--- src/views/PaginaInicial.vue
+++ src/views/PaginaInicial.vue
@@ -1,12 +1,31 @@
 <script>
-     import { RouterLink } from 'vue-router'
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
-    <div>
-        <h1>Aqui vai ter o botao de criar treino e ver treino</h1>
-        <RouterLink to="/cadastrarTreino">criar treino</RouterLink>
-        <br>
-        <RouterLink to="/treino">treino</RouterLink>
-    </div>
+  <div class="box">
+    <header>
+      <div class="cabecalho">
+        <img src="../../public/iconeMenu.svg" alt="">
+        <img src="../../public/logo.svg" alt="" />
+        <img src="../../public/iconeperfil.svg" alt="">
+      </div>
+    </header>
+    <menu>
+    <RouterLink to="/cadastrarTreino">criar treino</RouterLink>
+    <br />
+    <RouterLink to="/treino">treino</RouterLink>
+    </menu>
+  </div>
 </template>
+<style scoped>
+.cabecalho {
+  margin: 0 auto;  
+  text-align: center;
+  display: flex;
+  gap: 30px;
+  
+}
+
+
+</style>
