0b0449b142d728492192c493538aff1850b58c5e
Claudio Henrique <johnhenriqueoliveira2021@gmail.com>
2023-04-28 07:55:34 -0300

    Update server.js
    colequei o router em baixo. Como o ultimo app.use()

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,17 +1,17 @@
-const router = require('./roters')
-
 const express = require('express')
 const app = express()
 const port = 3000
+
+
 const cors = require('cors')
 app.use(cors())
-app.use(router())
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.status(200).json('Hello World!')
-})
+const router = require('./roters')
+app.use(router)
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
